b2d53ba6aa2dfe49361d0ee164bb3bb546b0d071
sumonsheikh <sm.sumon117@gmail.com>
2021-02-03 23:38:58 +0600

    create all pages

---
+++ src/pages/SingleCocktail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const  SingleCocktail = ()=> {
+    return (
+        <div>
+            <h2>Single Cocktail page</h2>
+        </div>
+    )
+}
+
+export default SingleCocktail
