06e7a6f891af40f3787805e04f4e5f47fc18a287
sumonsheikh <sm.sumon117@gmail.com>
2021-02-05 23:05:29 +0600

    create about us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const About = ()=> {
     return (
-        <div>
-            <h2>About us page</h2>
-        </div>
+        <section className="section about-sectoin">
+            <h1 className="section-title">About Us</h1>
+            <p>
+            Branch 'main' set up to track remote branch 'main' from 'origin'.
+            warning: LF will be replaced by CRLF in src/App.js.
+            The file will have its original line endings in your working directory
+            warning: LF will be replaced by CRLF in src/index.js.
+            The file will have its original line endings in your working directory
+            </p>
+        </section>
     )
 }
 
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Error =()=> {
     return (
         <div>
-                <h2>Error page</h2>
+                <h2> Error  page</h2>
         </div>
     )
 }
