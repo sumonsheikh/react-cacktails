c0452a527c85f3175826b8afe8df16d6860c367f
sumonsheikh <sm.sumon117@gmail.com>
2021-02-07 23:31:08 +0600

    add css for loading

--- src/component/CocktailList.js
+++ src/component/CocktailList.js
@@ -4,6 +4,12 @@ import Cocktail from './Cocktail'
 import Loading from './Loading'
 import {useGlobalContext} from '../context'
 const  CocktailList = () => {
+    const { cocktails, loading} = useGlobalContext()
+    if(loading){
+        return(
+            <Loading/>
+        )
+    }
     return (
         <div>
             <h2 >CocktailList component</h2>
--- src/component/Loading.js
+++ src/component/Loading.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Loading = () => {
     return (
-        <div>
-            <h1>Loading...</h1>
+        <div className="loader">
+           
         </div>
     )
 }
