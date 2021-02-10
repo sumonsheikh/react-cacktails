e5d1cd5fc2cab035b35ae760d4718ba2cfab7f42
sumonsheikh <sm.sumon117@gmail.com>
2021-02-10 22:19:13 +0600

    details btn in the cocktail list

--- src/component/Cocktail.js
+++ src/component/Cocktail.js
@@ -9,6 +9,9 @@ const  Cocktail = ({image, id, name, info, glass}) => {
                    <h3>{name}</h3>
                    <h3>{glass}</h3>
                    <p>{info}</p>
+                   <Link to={'/cocktail/${id}'} className ='btn btn-primary btn-details'>
+                       details
+                   </Link>
                </div>
            </div>
         </article>
