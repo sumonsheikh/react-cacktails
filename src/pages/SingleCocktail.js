ba457bb399f1adef51af862b2e7c4dc3fe253b8c
sumonsheikh <sm.sumon117@gmail.com>
2021-02-14 22:53:57 +0600

    create getCocktail fuction in use effect

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -6,6 +6,19 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
 const  SingleCocktail = ()=> {
     const {id } = useParams()
+    const [loading, setLoading ] = React.useState(false)
+    const [cocktail, setCocktail ] = React.useState(null)
+    React.useEffect(()=>{
+        setLoading(true);
+        async function getCocktail (){
+            try{
+                const response = await fetch(`${url}${url}`)
+                const data = await response.json();
+            }catch(error){
+                console.log(error);
+            }
+        }
+    },[id])
     return (
         <div>
             <h2>{id}</h2>
